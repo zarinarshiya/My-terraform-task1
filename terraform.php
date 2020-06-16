dad6fef4589383073baf331ce18e56f20848714a
Arshiya Zarin <65355152+zarinarshiya@users.noreply.github.com>
2020-06-16 17:39:44 +0530

    Create terraform.php

---
+++ terraform.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My The Amazon Web Services (AWS) provider is used to interact with the many resources supported by AWS. The provider needs to be configured with the proper credentials before it can be used.</p>
+
+</body>
+</html>
